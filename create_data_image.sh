3838fb0fc1b9f3a8875b29de498485b5fd62204f
Maarten ter Huurne <maarten@treewalker.org>
2012-10-17 20:25:12 +0200

    ext4 population: Allow different ways of running as super user
    Supported are sudo (as before), su and running the script as root.

--- create_data_image.sh
+++ create_data_image.sh
@@ -6,12 +6,33 @@ ROOTFS=../buildroot/output/images/rootfs.squashfs
 USER_UID=0
 USER_GID=0
 
+echo "Checking how to get superuser privileges..."
+if (( $UID == 0 ))
+then
+	echo "running as root"
+	SU_CMD="sh -c"
+else
+	echo -n "sudo: "
+	if which sudo
+	then
+		SU_CMD="sudo sh -c"
+	else
+		echo 'using "su"'
+		SU_CMD="su -c"
+	fi
+fi
+echo
+
 SIZE=$(stat -c %s ${ROOTFS})
-for app in apps/*
-do
-	SIZE=$((${SIZE} + $(stat -c %s ${app})))
-done
+if test -e apps/*
+then
+	for app in apps/*
+	do
+		SIZE=$((${SIZE} + $(stat -c %s ${app})))
+	done
+fi
 echo "Total data size: ${SIZE} bytes"
+echo
 
 # Pick a partition size that is large enough to contain all files but not much
 # larger so the image stays small.
@@ -21,15 +42,21 @@ echo "Creating data partition of ${IMAGE_SIZE} MB..."
 mkdir -p images
 dd if=/dev/zero of=images/data.bin bs=1M count=${IMAGE_SIZE}
 /sbin/mkfs.ext4 -m3 -O ^huge_file -F images/data.bin
+echo
 
 echo "Populating data partition..."
 echo "(this step needs superuser privileges)"
 mkdir mnt
-sudo mount images/data.bin mnt -o loop
-sudo install -m 644 -o 0 -g 0 ${ROOTFS} mnt/rootfs.bin
-sudo install -m 755 -o ${USER_UID} -g ${USER_GID} -d mnt/apps/
-sudo install -m 644 -o ${USER_UID} -g ${USER_GID} -t mnt/apps/ apps/*
-sudo install -m 755 -o 0 -g 0 -d mnt/local/etc/init.d
-sudo install -m 755 -o 0 -g 0 resize_data_part.target-sh mnt/local/etc/init.d/S00resize
-sudo umount mnt
+$SU_CMD "
+	mount images/data.bin mnt -o loop &&
+	install -m 644 -o 0 -g 0 ${ROOTFS} mnt/rootfs.bin &&
+	install -m 755 -o ${USER_UID} -g ${USER_GID} -d mnt/apps/ &&
+	if test -e apps/*
+	then
+		install -m 644 -o ${USER_UID} -g ${USER_GID} -t mnt/apps/ apps/*
+	fi &&
+	install -m 755 -o 0 -g 0 -d mnt/local/etc/init.d &&
+	install -m 755 -o 0 -g 0 resize_data_part.target-sh mnt/local/etc/init.d/S00resize &&
+	umount mnt
+	"
 rmdir mnt
