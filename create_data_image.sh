49e3dd149a0deca45f6dc6d8c240f0b63047fe0b
Maarten ter Huurne <maarten@treewalker.org>
2012-10-17 21:06:45 +0200

    Improved handling of files used to populate
    Explicitly check whether rootfs exists.
    Display files found and their sizes.
    Don't break if there is more than 1 application.
    Skip non-regular files in apps dir.

--- create_data_image.sh
+++ create_data_image.sh
@@ -23,20 +23,36 @@ else
 fi
 echo
 
-SIZE=$(stat -Lc %s ${ROOTFS})
-if test -e apps/*
+echo "Gathering rootfs and applications..."
+if test -f ${ROOTFS}
 then
-	for app in apps/*
-	do
-		SIZE=$((${SIZE} + $(stat -Lc %s ${app})))
-	done
+	SIZE=$(stat -Lc %s ${ROOTFS})
+	echo "rootfs: ${ROOTFS} - $((${SIZE} / 1024)) kB"
+	TOTAL_SIZE=${SIZE}
+else
+	echo "missing rootfs: ${ROOTFS}"
+	exit 1
 fi
-echo "Total data size: ${SIZE} bytes"
+APPS=
+for app in apps/*
+do
+	if test -f $app
+	then
+		APPS="${APPS} $app"
+		SIZE=$(stat -Lc %s ${app})
+		TOTAL_SIZE=$((${TOTAL_SIZE} + ${SIZE}))
+		echo "app: ${app} - $((${SIZE} / 1024)) kB"
+	elif [ $app != "apps/*" ]
+	then
+		echo "skipping non-regular file: $app"
+	fi
+done
+echo "Total data size: ${TOTAL_SIZE} bytes"
 echo
 
 # Pick a partition size that is large enough to contain all files but not much
 # larger so the image stays small.
-IMAGE_SIZE=$((8 + ${SIZE} / (960*1024)))
+IMAGE_SIZE=$((8 + ${TOTAL_SIZE} / (960*1024)))
 
 echo "Creating data partition of ${IMAGE_SIZE} MB..."
 mkdir -p images
@@ -47,13 +63,13 @@ echo
 echo "Populating data partition..."
 echo "(this step needs superuser privileges)"
 mkdir mnt
-$SU_CMD "
+${SU_CMD} "
 	mount images/data.bin mnt -o loop &&
 	install -m 644 -o 0 -g 0 ${ROOTFS} mnt/rootfs.bin &&
 	install -m 755 -o ${USER_UID} -g ${USER_GID} -d mnt/apps/ &&
-	if test -e apps/*
+	if [ \"${APPS}\" != \"\" ]
 	then
-		install -m 644 -o ${USER_UID} -g ${USER_GID} -t mnt/apps/ apps/*
+		install -m 644 -o ${USER_UID} -g ${USER_GID} -t mnt/apps/ ${APPS}
 	fi &&
 	install -m 755 -o 0 -g 0 -d mnt/local/etc/init.d &&
 	install -m 755 -o 0 -g 0 resize_data_part.target-sh mnt/local/etc/init.d/S00resize &&
