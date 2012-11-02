51ceec09f0859aa65273733a11d8476d550bad90
Maarten ter Huurne <maarten@treewalker.org>
2012-11-02 12:09:27 +0100

    Switch boot loader from u-boot to UBIBoot

--- create_boot_image.sh
+++ create_boot_image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Partition start sectors.
-KERNEL_START=8192
+KERNEL_START=32
 DATA_START=32768
 
 echo "Creating boot sector..."
 mkdir -p temp
 ./genmbr.py > temp/bootsector.bin <<EOF
-${KERNEL_START},$((${DATA_START} - ${KERNEL_START})),f0
+${KERNEL_START},$((${DATA_START} - ${KERNEL_START})),0b
 ${DATA_START},$((800 * 1024 * 2)),83
 EOF
 
@@ -16,6 +16,5 @@ mkdir -p images
 dd if=/dev/zero of=images/boot.bin bs=512 count=${KERNEL_START} status=noxfer
 dd seek=0  if=temp/bootsector.bin of=images/boot.bin conv=notrunc bs=512 status=noxfer
 dd seek=16 if=temp/bootsector.bin of=images/boot.bin conv=notrunc bs=512 status=noxfer
-dd seek=1  if=uboot-stage1.bin of=images/boot.bin conv=notrunc bs=512 status=noxfer
-dd seek=17 if=uboot-stage1.bin of=images/boot.bin conv=notrunc bs=512 status=noxfer
-dd seek=33 if=uboot-stage2.bin of=images/boot.bin conv=notrunc bs=512 status=noxfer
+dd seek=1  if=ubiboot.bin of=images/boot.bin conv=notrunc bs=512 status=noxfer
+dd seek=17 if=ubiboot.bin of=images/boot.bin conv=notrunc bs=512 status=noxfer
