0f9707d60535cd6f0428f3ca6cb1ef2689dad48a
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-04-03 01:27:18 +0100

    create_data_image.sh: Do not require root
    Signed-off-by: Gleb Mazovetskiy <glex.spb@gmail.com>

--- create_data_image.sh
+++ create_data_image.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-# Currently everything runs as root, but that is going to change.
-USER_UID=0
-USER_GID=0
-
-source ./su_command.sh
-
 echo "Gathering applications..."
 mapfile -d $'\0' APPS < <(find apps/ -maxdepth 1 -type f -name '*.opk' -print0)
 if (( ${#APPS[@]} )); then
@@ -20,6 +14,16 @@ else
 fi
 echo
 
+echo "Preparing data for the data partition..."
+rm -rf tmp-data
+mkdir tmp-data
+install -m 755 -d tmp-data/apps
+if (( ${#APPS[@]} )); then
+	install -m 644 -t tmp-data/apps/ "${APPS[@]}"
+fi
+install -m 755 -d tmp-data/local/etc/init.d
+install -m 755 resize_data_part.target-sh tmp-data/local/etc/init.d/S00resize
+
 # Pick a partition size that is large enough to contain all files but not much
 # larger so the image stays small.
 IMAGE_SIZE=$((8 + APPS_SIZE / (920*1024)))
@@ -27,20 +31,6 @@ IMAGE_SIZE=$((8 + APPS_SIZE / (920*1024)))
 echo "Creating data partition of ${IMAGE_SIZE} MB..."
 mkdir -p images
 dd if=/dev/zero of=images/data.bin bs=1M count=${IMAGE_SIZE}
-MKE2FS_CONFIG=mke2fs.conf /sbin/mke2fs -t od-data -F images/data.bin
-echo
-
-echo "Populating data partition..."
-echo "(this step needs superuser privileges)"
-mkdir mnt
-${SU_CMD} "
-	mount images/data.bin mnt -o loop &&
-	install -m 755 -o ${USER_UID} -g ${USER_GID} -d mnt/apps/ &&
-	if [ ${#APPS[@]} -gt 0 ]; then
-		install -m 644 -o ${USER_UID} -g ${USER_GID} -t mnt/apps/ $(printf "%q " "${APPS[@]}")
-	fi &&
-	install -m 755 -o 0 -g 0 -d mnt/local/etc/init.d &&
-	install -m 755 -o 0 -g 0 resize_data_part.target-sh mnt/local/etc/init.d/S00resize &&
-	umount mnt
-	"
-rmdir mnt
+MKE2FS_CONFIG=mke2fs.conf /sbin/mke2fs -t od-data \
+  -d tmp-data -F images/data.bin
+rm -rf tmp-data
